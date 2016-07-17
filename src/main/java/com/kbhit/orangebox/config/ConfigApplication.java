c918fcbf09d4bed46dc3a3d76ad645a02717fc00
kbhit <gurgul.grzegorz@gmail.com>
2016-07-17 18:52:11 +0200

    Trigger travis

--- src/main/java/com/kbhit/orangebox/config/ConfigApplication.java
+++ src/main/java/com/kbhit/orangebox/config/ConfigApplication.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigServer
 public class ConfigApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
     }
